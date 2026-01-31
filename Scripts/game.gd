d4a4a353ff7c591f8717e157929b8c0e8e94174c
JanMaymi <jan.maymi@gmail.com>
2026-01-31 12:06:56 +0100

    mega

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -1,14 +1,12 @@
 extends Node
 
-
-
-func _on_button_menu_pressed() -> void:
+func _on_texture_button_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
 
-func _on_button_map_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/map.tscn")
-
-
-func _on_button_book_pressed() -> void:
+func _on_texture_button_book_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/rules.tscn")
+
+
+func _on_texture_button_map_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/map.tscn")
--- Scripts/map.gd
+++ Scripts/map.gd
@@ -1,6 +1,4 @@
 extends Node
 
-
-
-func _on_button_return_pressed() -> void:
+func _on_texture_button_return_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/game.tscn")
