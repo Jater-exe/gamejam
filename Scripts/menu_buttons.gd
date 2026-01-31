b005fc9733db40a416b9fff1f084fccdab2a8b07
Jater-exe <jter6868@gmail.com>
2026-01-31 11:54:53 +0100

    Revert to buttons commit
    This reverts commit 78dcfe9bc11521d51df8f62521b2b06548216f34, reversing
    changes made to aaec1a66b08a54fb4fd16a2cc555bea9f97644f4.

--- Scripts/menu_buttons.gd
+++ Scripts/menu_buttons.gd
@@ -1,7 +1,10 @@
 extends VBoxContainer
 
-func _on_texture_button_exit_pressed() -> void:
+
+
+func _on_button_exit_pressed() -> void:
 	get_tree().quit()
 
-func _on_texture_button_start_pressed() -> void:
+
+func _on_button_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/setting.tscn")
