ea9582339651d0b500731735da33b49dae31e6fe
JanMaymi <jan.maymi@gmail.com>
2026-01-31 12:40:11 +0100

    prova2

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -4,17 +4,26 @@ extends Node
 =======
 
 
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 func _on_texture_button_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 =======
+=======
+>>>>>>> Stashed changes
 func _on_texture_button_map_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/map.tscn")
 
 
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 func _on_texture_button_book_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/rules.tscn")
