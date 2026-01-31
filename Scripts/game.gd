2bbb00a576625d86da6c58694d03dae768254cd3
diegovilbe <diego.villbe@gmail.com>
2026-01-31 20:04:25 +0100

    el malo

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -2,17 +2,21 @@ extends Node
 
 var times = 0
 @onready var bell_sound = $Button_Bell/audiocampana
-
 var state:int = 0
 
-var scene
+var scene = preload("res://Scenes/map.tscn")
 var instance
-
 func _on_texture_button_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
 func _on_texture_button_map_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/map.tscn")
+	if has_node("mapita") :
+		instance.queue_free()
+		return
+	var instance = scene.instantiate()
+	instance.name = "mapita"
+	add_child(instance)
+	instance.z_index=10
 
 func _on_texture_button_book_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/rules.tscn")
--- Scripts/map.gd
+++ Scripts/map.gd
@@ -1,8 +1,10 @@
 extends Node
-
+var tocat : bool=false
 func _on_texture_button_return_pressed() -> void:
+	tocat=false
 	get_tree().change_scene_to_file("res://Scenes/game.tscn")
 	
 func _input(event: InputEvent) -> void:
+	tocat=true
 	if Input.is_action_just_pressed("esc"):
 		get_tree().change_scene_to_file("res://Scenes/game.tscn")
