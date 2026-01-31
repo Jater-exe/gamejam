7181d03e01cf30c4c1350ce029c0e72a60f500fc
JanMaymi <jan.maymi@gmail.com>
2026-01-31 11:40:56 +0100

    buttons

--- Scripts/control.gd
+++ Scripts/control.gd
@@ -28,6 +28,11 @@ func write_text():
 	
 	
 
-
 func _on_timer_timeout() -> void:
 	get_tree().change_scene_to_file("res://Scenes/game.tscn")
+
+func _input(event):
+	if event.is_action_pressed("space"):
+		$Timer.stop()
+		get_tree().change_scene_to_file("res://Scenes/game.tscn")
+	
--- Scripts/menu_buttons.gd
+++ Scripts/menu_buttons.gd
@@ -1,10 +1,7 @@
 extends VBoxContainer
 
-
-
-func _on_button_exit_pressed() -> void:
+func _on_texture_button_exit_pressed() -> void:
 	get_tree().quit()
 
-
-func _on_button_start_pressed() -> void:
+func _on_texture_button_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/setting.tscn")
