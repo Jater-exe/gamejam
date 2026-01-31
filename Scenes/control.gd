296a3e107432dc3086b560b53979c345321faae7
diegovilbe <diego.villbe@gmail.com>
2026-01-31 10:54:14 +0100

    texto inicial

---
+++ Scenes/control.gd
@@ -0,0 +1,26 @@
+extends Control
+
+@onready var text_label = $RichTextLabel
+@onready var typing_sound = $typingsound
+
+var full_text := "You are a custom's agent. 
+Your task is to either deny or accept the entries.
+Follow the information to do so.."
+var char_index := 0
+var speed := 0.05  # segundos entre letras
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	text_label.text = ""
+	typing_sound.play()
+	write_text()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func write_text():
+	while char_index < full_text.length():
+		text_label.text += full_text[char_index]
+		char_index += 1
+		
+		# Reproducir el sonido
+		
+		await get_tree().create_timer(speed).timeout
