be0ce394d414a662cda7211184441065794c9a48
Jater-exe <jter6868@gmail.com>
2026-01-31 18:12:56 +0100

    Character almost finished. Must polish aesthetic details

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -1,16 +1,26 @@
 extends Node
 
-@onready var _animated_sprite = $AnimatedSprite2D
+@onready var _animated_sprite = self
 @export var time: float = 0.0
 
 func _ready() -> void:
+	if has_node("Mask"):
+		return
+	var scene = preload("res://Scenes/mask.tscn")
+	var instance = scene.instantiate()
+	instance.name = "Mask"
+	add_child(instance)
+	instance.position = Vector2(-215, -770)
 	_animated_sprite.play()
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if time >= 0.25:
+func _process(delta: float) -> void:		
+	if _animated_sprite.scale.x >= 1:
+		_animated_sprite.stop()
+	if time >= 0.25 and _animated_sprite.scale.x < 1:
 		_animated_sprite.scale.x += 0.1
 		_animated_sprite.scale.y += 0.1
 		time = delta
+	elif _animated_sprite.scale.x < 1:
+		time += delta
