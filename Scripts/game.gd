3921ad3d1786f9222a0b9380402a50c153238212
JanMaymi <jan.maymi@gmail.com>
2026-01-31 16:13:35 +0100

    Update game.gd

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -19,7 +19,9 @@ func _input(event: InputEvent) -> void:
 		get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
 func _on_texture_button_accept_pressed() -> void:
-	pass # Replace with function body.
+	pass
 
 func _on_texture_button_deny_pressed() -> void:
-	pass # Replace with function body.
+	pass
+
+	
