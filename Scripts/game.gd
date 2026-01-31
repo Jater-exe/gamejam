422949105c72536489f2c65cdd71078a213185ee
JanMaymi <jan.maymi@gmail.com>
2026-01-31 13:21:15 +0100

    inputs

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -4,10 +4,17 @@ extends Node
 func _on_texture_button_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
-
 func _on_texture_button_map_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/map.tscn")
 
-
 func _on_texture_button_book_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/rules.tscn")
+
+func _input(event: InputEvent) -> void:
+	if event.is_action("book"):
+		get_tree().change_scene_to_file("res://Scenes/rules.tscn")
+	elif event.is_action("map"):
+		get_tree().change_scene_to_file("res://Scenes/map.tscn")
+	elif Input.is_action_just_pressed("esc"):
+		get_tree().change_scene_to_file("res://Scenes/main.tscn")
+ 
--- rules.gd
+++ rules.gd
@@ -2,3 +2,7 @@ extends Node
 
 func _on_texture_button_return_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/game.tscn")
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("esc"):
+		get_tree().change_scene_to_file("res://Scenes/game.tscn")
