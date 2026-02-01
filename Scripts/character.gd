abd56515fa7ecd7dd1e911bf0240b5ed9867d837
Jater-exe <jter6868@gmail.com>
2026-02-01 11:55:18 +0100

    Implementing dni pt1

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -2,6 +2,8 @@ extends Node
 
 @onready var _animated_sprite = self
 @export var time: float = 0.0
+var scene_dni
+var instance_dni
 
 func _ready() -> void:
 	if has_node("Mask"):
@@ -18,6 +20,11 @@ func _ready() -> void:
 func _process(delta: float) -> void:		
 	if _animated_sprite.scale.x >= 1:
 		_animated_sprite.stop()
+		scene_dni = preload("res://Scenes/dni.tscn")
+		var instance_dni = scene_dni.instantiate()
+		instance_dni.name = "dni"
+		add_child(instance_dni)
+		instance_dni.position = Vector2(180,50)
 	if time >= 0.25 and _animated_sprite.scale.x < 1:
 		_animated_sprite.scale.x += 0.1
 		_animated_sprite.scale.y += 0.1
