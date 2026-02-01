f909d4c03a3caa0de5446e75bf834a401d14224f
Jater-exe <jter6868@gmail.com>
2026-02-01 11:11:40 +0100

    Gamethings added

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -22,6 +22,35 @@ func _on_texture_button_menu_pressed() -> void:
 	add_child(instancepaused)
 	get_tree().paused = true
 
+@onready var places_mask
+
+const PLACES_MASKS_PATH = "res://Assets/Text/places_masks.json"
+
+
+func load_json(path: String) -> Variant:
+	var file = FileAccess.open(path, FileAccess.READ)
+	if file == null:
+		push_error("Failed to open JSON file")
+		return null
+
+	var json_text = file.get_as_text()
+	file.close()
+
+	var json = JSON.new()
+	var error = json.parse(json_text)
+
+	if error != OK:
+		push_error("JSON parse error")
+		return null
+
+	return json.data
+
+func is_bad(person: Dictionary) -> bool:
+	if person["Mask"] not in places_mask[person["Birthplace"]]:
+		return false
+	else:
+		return true
+
 func _on_texture_button_map_pressed() -> void:
 	if state == 2 or state == 0:
 		if has_node("big_map") :
